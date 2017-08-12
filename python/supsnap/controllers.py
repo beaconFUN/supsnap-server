13b6b1ebfe143ddfdc72d5dec30b069abdddf0e5
Takuya Shizukuishi <sititou70@gmail.com>
2017-08-12 12:09:55 +0900

    [add]define models

--- python/supsnap/controllers.py
+++ python/supsnap/controllers.py
@@ -1,22 +1,8 @@
 from flask import Flask, request, flash, url_for, redirect, render_template
 from app import app
 from db import db
-from models import students
+from models import Beacon, Place, Visiter, Snap
 
-@app.route('/')
+@app.route("/")
 def show_all():
-    return render_template('show_all.html', students = students.query.all() )
-
-@app.route('/new', methods = ['GET', 'POST'])
-def new():
-    if request.method == 'POST':
-        if not request.form['name'] or not request.form['city'] or not request.form['addr']:
-            flash('Please enter all the fields', 'error')
-        else:
-            student = students(request.form['name'], request.form['city'], request.form['addr'], request.form['pin'])
-            
-            db.session.add(student)
-            db.session.commit()
-            flash('Record was successfully added')
-            return redirect(url_for('show_all'))
-    return render_template('new.html')
+    return render_template("index.html")
